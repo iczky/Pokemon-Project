5caad5881483f963b8a443c717f2a06caa6f3d69
iczky <fiqra22@gmail.com>
2024-05-03 14:50:53 +0700

    fix: Fix detail page with the usage of new Context State

--- src/pages/DetailPage/index.tsx
+++ src/pages/DetailPage/index.tsx
@@ -1,27 +1,35 @@
-import Navbar from "../../components/Navbar";
-import StatCard from "./StatCard";
-import usePokemonDetails from "../../hooks/usePokemonDetail";
 import { useParams } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import { usePokemonContext } from "../../hooks/PokemonListContext";
+import StatCard from "./StatCard";
 
 const DetailPage = () => {
-  const { id } = useParams();
-  const { pokemonDetails } = usePokemonDetails(id);
+  const { id } = useParams<{ id: string | undefined }>();
+
+  const context = usePokemonContext()?.pokemons;
+
+  const filteredPokemon =
+    context?.find((pokemon) => pokemon.name === id) || null;
+  console.log(filteredPokemon, "=============filtered==============+");
+
+  if (filteredPokemon === null) return <div>Loading...</div>;
+
   return (
     <>
       <Navbar searchIconProps={false} />
       <div className="max-w-screen-sm px-7 py-7 bg-base-color flex flex-col text-left gap-2">
         <h2 className="text-lg font-lato text-base-id-poke">
-          {`# ${pokemonDetails?.id}`}
+          {`# ${filteredPokemon.id}`}
         </h2>
-        <img src={pokemonDetails?.artworkFront} alt="Pokemon Image" />
+        <img src={filteredPokemon.artworkFront} alt="Pokemon Image" />
         <div className="flex justify-between items-center text-white font-lato text-4xl">
-          <h1 className="">{pokemonDetails?.name}</h1>
-          <img src={pokemonDetails?.spriteFront} alt="Sprite Pokemon" />
+          <h1 className="">{filteredPokemon.name}</h1>
+          <img src={filteredPokemon.spriteFront} alt="Sprite Pokemon" />
         </div>
         <StatCard
-          defense={pokemonDetails?.defense}
-          health={pokemonDetails?.health}
-          attack={pokemonDetails?.attack}
+          defense={filteredPokemon.defense}
+          health={filteredPokemon.health}
+          attack={filteredPokemon.attack}
         />
       </div>
     </>
